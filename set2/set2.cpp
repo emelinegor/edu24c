16ff92f13f3b9846cc855f1812ba6a96ff8a4015
Емелин Егор <egoremelinegor@gmail.com>
2024-11-09 11:01:08 +0300

    09112024_1059
     set2 variable

---
+++ set2/set2.cpp
@@ -0,0 +1,20 @@
+
+/*
+set 2
+Student - Emelin Egor
+09112024
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 2;
+    a = a + 1;
+    int result = a - b;
+
+    cout << result << endl;
+
+    return 0;
+}
