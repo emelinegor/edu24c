26026507292fd80343cef752b4225783cc80159a
Емелин Егор <egoremelinegor@gmail.com>
2024-11-16 10:40:31 +0300

    16112_ 1040
    tricks

---
+++ tricks/tricks.cpp
@@ -0,0 +1,20 @@
+/*
+Student - Emelin Egor
+16112024
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int x = 45;
+int y = 54;
+int z = (x < y) ? x : y;
+
+int32_t main()
+{
+
+cout << z << endl;
+
+    return 0;
+}
