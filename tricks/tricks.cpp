7bb0cb8d98d54ad007f8398f42f7fab67862c729
Емелин Егор <egoremelinegor@gmail.com>
2024-11-16 10:43:12 +0300

    161124_1043
    tricks

--- tricks/tricks.cpp
+++ tricks/tricks.cpp
@@ -5,6 +5,7 @@ Student - Emelin Egor
 #include <bits/stdc++.h>
 using namespace std;
 
+#define omg cout << "OMG!" << "\n"
 #define int long long
 
 int x = 45;
@@ -13,8 +14,7 @@ int z = (x < y) ? x : y;
 
 int32_t main()
 {
-
 cout << z << endl;
-
+omg;
     return 0;
 }
