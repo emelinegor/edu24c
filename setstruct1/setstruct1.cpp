b747d464ff8a0661d2e92240e2b1f3a36c835abe
Емелин Егор <egoremelinegor@gmail.com>
2024-11-30 12:04:22 +0300

    301124_1204
    set struct 1

---
+++ setstruct1/setstruct1.cpp
@@ -0,0 +1,24 @@
+/*
+set struct 1
+Student - Emelin Egor
+30112024
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Employee
+{
+    string names;
+    long int salary;
+    int age;
+};
+Employee john {"John", 45000, 36};
+Employee milla {"Milla", 25000};
+
+int main() 
+{
+cout << "Имя: " << john.names << " " << "Зарплата: " << john.salary << " " << "Возвраст: " << john.age << endl;
+cout << "Имя: " << milla.names << " " << "Зарплата: " <<  milla.salary << " " << endl;
+    return 0;
+}
