83ba844bb66cfc80b94511736dd7a5f1844906c3
Емелин Егор <egoremelinegor@gmail.com>
2024-12-13 17:55:03 +0300

    131224

---
+++ set1struct/set1struct.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct student
+{
+    string name;
+    int groupNumber;
+    double averengeGrade;
+};
+
+double result = 0.0;
+
+student MGU[] =
+{
+ {"Masha", 3, 3.5},
+ {"Alex", 5, 4.3},
+ {"Vanya", 23, 4.5},
+ {"Dasha", 12, 4.7},
+ {"Pasha", 32, 4.9},
+};
+
+
+
+
+
+
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+    result = result + MGU[i].averengeGrade;
+    }
+    double averenge = result / 5;
+
+
+    double range[4] = {};
+    for (int i = 0; i < 5; i++)
+    {
+        double range[i] = MGU[i].averengeGrade;
+    }
+    for (int j = 1; j < 5; j++)
+    {
+        for (int i = 0; i < 5; i++)
+        if(range[i] > range[i + 1])
+        {
+            double x = range[i];
+            range[i] = range[i + 1];
+            range[i + 1] = x;
+        }
+    }
+
+
+    cout << averenge;
+    for (int i = 0; i < 5; i++)
+        {
+            cout << range[i];
+        }
+
+   return 0;
+}
